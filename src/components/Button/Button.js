a57431af2ad9aa94c5f77046ad9612fa693b3603
Luca Valsecchi <luca.valsecchi@i-seed.it>
2018-05-09 15:05:38 +0200

    add some logic

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,12 @@
+import PropTypes from 'prop-types'
 import './Button.css';
 import React from 'react';
 
-export const Button = () => {
-    return <button>ciao</button>
+export const Button = (props) => {
+    return <button onClick={props.customOnClick}>{props.title}</button>
+};
+
+Button.propTypes = {
+    title: PropTypes.string,
+    customOnClick: PropTypes.func
 };
